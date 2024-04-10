199964ac527f8f15eb572b84404cd1a71f3a6da0
Kazuki Ota <kaota@microsoft.com>
2024-04-10 18:23:15 +0900

    プロジェクト ファイルを追加します。

---
+++ SmartComponentsJp/Models/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SmartComponentsJp.Models;
+
+public class Employee
+{
+    [Required(ErrorMessage = "名前を入力してください。")]
+    public string Name { get; set; } = "";
+    [Required(ErrorMessage = "郵便番号を入力してください。")]
+    public string ZipCode { get; set; } = "";
+    [Required(ErrorMessage = "住所を入力してください。")]
+    public string Address { get; set; } = "";
+    [Required(ErrorMessage = "電話番号を入力してください。")]
+    public string PhoneNumber { get; set; } = "";
+    [Required(ErrorMessage = "メールアドレスを入力してください。")]
+    public string EmailAddress { get; set; } = "";
+}
---
+++ SmartComponentsJp/Utils/Verifier.cs
@@ -0,0 +1,10 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace SmartComponentsJp.Utils;
+
+public static class Verifier
+{
+    public static void Verify(
+        [NotNull] object? value) =>
+        ArgumentNullException.ThrowIfNull(value);
+}
